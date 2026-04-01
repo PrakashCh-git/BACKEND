39e93d286555f2f46a1986da8fa74de9e96d265c
Prakash <prakashleetcode2003@gmail.com>
2026-04-01 16:30:29 +0530

    Integrated frontend and backend

--- day-7/Backend/src/app.js
+++ day-7/Backend/src/app.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const noteModel = require('./models/note-model');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(express.static("./public"));
+
+
+
 
 
 app.post('/api/note',async (req,res)=>{
@@ -49,6 +54,10 @@ app.delete('/api/note/:id',async (req,res)=>{
     res.status(200).json({
         message:"Note deleted successfully"
     })
+})
+
+app.use('*name',(req,res)=>{
+    res.sendFile(path.join(__dirname,"..","/public/index.html"));
 })
 
 
