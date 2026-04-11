fbd9d10b6474e4a68ff910d8a84b5bcd700f7b67
Prakash <prakashleetcode2003@gmail.com>
2026-04-11 18:07:26 +0530

    Finishing a basic authentication

--- day-8/src/Routes/auth.route.js
+++ day-8/src/Routes/auth.route.js
@@ -44,8 +44,55 @@ authRouter.post('/register', async (req,res) =>{
         token
     })
 
-
-    
 })
+
+    //Creating a protected route
+    authRouter.post("/protected", (req, res) =>{
+        console.log(req.cookies);
+
+        res.status(200).json({
+            message: "This is a protected route"
+        })
+    })
+
+    //Creating login controller
+    authRouter.post("/login", async(req,res)=>{
+
+        //Destructuring user details
+        const {email,password} = req.body;
+
+        //Verifying the existance of the email address
+        const user = await userModel.findOne({email});
+        if(!user) {
+            return res.status(404).json({
+                message:"Email address doesn't exist"
+            })
+        }
+
+        //Verifying password 
+        const isPasswordCorrect = user.password === password;
+        if(!isPasswordCorrect) {
+            return res.status(401).json({
+                message: "Invalid Password"
+            })
+        }
+
+        //Creating token if credentials match
+        const token = jwt.sign(
+            {
+                id: user._id
+            },
+            process.env.JWT_SECRET
+        )
+
+        //Saving token in user browser's cookies
+        res.cookie("jwt_token",token);
+
+        //Final response
+        res.status(200).json({
+            message: "User logged successfully",
+            user
+        })
+    })
 
 module.exports = authRouter;
